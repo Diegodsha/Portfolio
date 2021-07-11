445d2aa40a004e0e0d3f45ea89a76a3a7f39ba5d
Diego <diegodsha@hotmail.com>
2021-07-10 20:51:21 -0500

    Update Of projects and styles

--- index.js
+++ index.js
@@ -103,6 +103,18 @@ t1.from(
   "-=.7"
 );
 
+// gsap.from(".projectsinfo6", {
+//   scrollTrigger: {
+//     trigger: ".projectsinfo6",
+//     start: "bottom bottom",
+//     toggleActions: "restart none restart none",
+//     // markers: true,
+//   },
+//   y: -115,
+//   opacity: 1,
+//   duration: 1.2,
+//   stagger: 0.3,
+// });
 
 gsap.from(".projectsinfo1", {
   scrollTrigger: {
@@ -110,7 +122,7 @@ gsap.from(".projectsinfo1", {
     start: "bottom bottom",
     toggleActions: "restart none restart none",
   },
-  y: -50,
+  y: -115,
   opacity: 1,
   duration: 1.2,
   stagger: 0.3,
@@ -122,7 +134,7 @@ gsap.from(".projectsinfo2", {
     start: "bottom bottom",
     toggleActions: "restart none restart none",
   },
-  y: -50,
+  y: -115,
   opacity: 1,
   duration: 1.2,
   stagger: 0.3,
@@ -134,7 +146,7 @@ gsap.from(".projectsinfo3", {
     start: "bottom bottom",
     toggleActions: "restart none restart none",
   },
-  y: -50,
+  y: -115,
   opacity: 1,
   duration: 1.2,
   stagger: 0.3,
@@ -146,11 +158,37 @@ gsap.from(".projectsinfo4", {
     start: "bottom bottom",
     toggleActions: "restart none restart none",
   },
-  y: -50,
+  y: -115,
   opacity: 1,
   duration: 1.2,
   stagger: 0.3,
 });
+
+gsap.from(".projectsinfo5", {
+  scrollTrigger: {
+    trigger: ".projectsinfo5",
+    start: "bottom bottom",
+    toggleActions: "restart none restart none",
+  },
+  y: -115,
+  opacity: 1,
+  duration: 1.2,
+  stagger: 0.3,
+});
+
+// gsap.from(".projectsbtn6", {
+//   scrollTrigger: {
+//     trigger: ".projectsbtn6",
+//     start: "top bottom ",
+//     toggleActions: "restart none restart none",
+//     // markers: true,
+//   },
+//   y: 50,
+//   opacity: 0,
+//   duration: 1.2,
+//   stagger: 0.3,
+// });
+
 
 gsap.from(".projectsbtn1", {
   scrollTrigger: {
@@ -191,6 +229,18 @@ gsap.from(".projectsbtn3", {
 gsap.from(".projectsbtn4", {
   scrollTrigger: {
     trigger: ".projectsbtn4",
+    start: "top bottom ",
+    toggleActions: "restart none restart none",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: 0.3,
+});
+
+gsap.from(".projectsbtn5", {
+  scrollTrigger: {
+    trigger: ".projectsbtn5",
     start: "top bottom ",
     toggleActions: "restart none restart none",
   },
@@ -268,9 +318,9 @@ gsap.from(".skill", {
 var t2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".svg-skill-1",
-    //markers: true,
+    // markers: true,
     start: "bottom bottom",
-    end: "top 9%",
+    end: "top -50%",
     scrub: 1.8,
     toggleActions: "restart none restart none",
   },
@@ -306,7 +356,7 @@ t2.from(
     duration: 2,
     ease: Power4.easeOut,
   },
-  "-=1.5"
+  "-=1.2"
 );
 
 t2.from(
@@ -391,6 +441,86 @@ t2.from(
 );
 t2.from(
   ".svg-skill-12",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-13",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-14",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-15",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-16",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-17",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-18",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-19",
+  {
+    x: "50%",
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=1"
+);
+t2.from(
+  ".svg-skill-20",
   {
     x: "50%",
     opacity: 0,
